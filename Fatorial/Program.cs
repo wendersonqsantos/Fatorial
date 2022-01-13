a5f265d273adb314cfb3c3d51823b9d13ae3e45f
wendersonqsantos <wendersonqsantos@gmail.com>
2022-01-13 08:59:01 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        public static void imprimir (int numero, int fatorial)
+        {
+            Console.WriteLine("Fatorial de: "+numero+" é: "+fatorial);
+        }
+        public static int fatorial(int numero)
+        {
+            if (numero == 1)
+            {
+                return 1;
+            }
+            else return numero * fatorial(numero - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            int numero,fatorial_;
+            numero = fatorial_ = 0;
+            Console.WriteLine("Informe um número para ser calculado o seu fatorial: ");
+            numero = int.Parse(Console.ReadLine());
+           fatorial_= fatorial(numero);
+            imprimir(numero, fatorial_);
+            Console.ReadKey();
+        }
+    }
+}
